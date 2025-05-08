26197597bd986c9f7c78da3cbe0aafeb1f45e30c
nisharajp <nishaarajput.20@gmail.com>
2025-05-08 18:08:26 +0530

    Initial commit

---
+++ src/ex_java_01_basic.java
@@ -0,0 +1,6 @@
+public class ex_java_01_basic {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/largest_number.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class largest_number {
+
+    public static void main(String[] args) {
+
+        int a= 90;
+        int b= 60;
+
+        int largestNumber= (a>b)?a:b;
+
+        System.out.println("largest Number:" + largestNumber);
+    }
+
+
+}
